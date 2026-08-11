cc0f8247cf9820e43a277fd7a69a438de7d24b9f
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-08-11 07:50:16 +0530

    Time: 2 ms (12.67%) | Memory: 15.2 MB (49.48%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -11,18 +11,35 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<vector<int>>& ans, int level) {
-        if (root == NULL) return;
-        if (ans.size() == level) ans.push_back({});
-        ans[level].push_back(root->val);
-        helper(root->left, ans, level + 1); helper(root->right, ans, level + 1);
-    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        helper(root, ans, 0);
-        for (int i = 0; i < ans.size(); i++) {
-            if (i % 2 != 0) reverse(ans[i].begin(), ans[i].end());
+        stack<TreeNode*> st1; //left to right
+        stack<TreeNode*> st2; //right to left
+        if(root==NULL) return ans; st1.push(root);
+        while(st1.size() || st2.size()){
+            vector<int> v;
+            //left to right
+            while(st1.size()>0){
+                TreeNode* temp = st1.top();
+                st1.pop();
+                v.push_back(temp->val);
+
+                if(temp->left) st2.push(temp->left);
+                if(temp->right) st2.push(temp->right);
+            }
+            if(v.size()>0) ans.push_back(v);
+            v.clear();
+            //right to left
+            while(st2.size()>0){
+                TreeNode* temp = st2.top();
+                st2.pop();
+                v.push_back(temp->val);
+                if(temp->right) st1.push(temp->right);
+                if(temp->left) st1.push(temp->left);
+            }
+            if(v.size()>0) ans.push_back(v);
+            v.clear();
         }
-        return ans;
+        return ans;   
     }
 };
