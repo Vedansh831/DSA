6e777ade79c1dd0cc2917246275d21f983847af8
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-08-15 01:57:53 +0530

    Time: 32 ms (7.21%) | Memory: 23.2 MB (99.51%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,12 +10,22 @@
 
 class Solution {
 public:
+    bool find(TreeNode* root,TreeNode* target){
+        if(root==NULL) return false;
+        if(root==target) return true;
+        return find(root->right,target) || find(root->left,target);
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL || root==p || root==q) return root;
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);   
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);  
-        if(left && right) return root;
-        if(!right) return left;
-        return right; 
+        // if(root==p || root==q) return root;
+        // else if (find(root->left,p)==true && find(root->right,q)==true) return root;
+        // else if (!find(root->left,p) && !find(root->right,q)) return root;
+        // else if (find(root->left,p)==true && !find(root->right,q)) return lowestCommonAncestor(root->left,p,q);
+        // else return lowestCommonAncestor(root->right,p,q);
+        //Better Method
+        if(root==p || root==q) return root;
+        if(root->val > p->val && root->val < q->val) return root;
+        else if(root->val < p->val && root->val > q->val) return root;
+        else if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+        else return lowestCommonAncestor(root->left,p,q);
     }
 };
