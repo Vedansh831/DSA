08434fc2b491563dc0ad89b502a78f02d4dcd910
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-08-14 16:14:50 +0530

    Time: 92 ms (5.62%) | Memory: 222.3 MB (5.49%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,22 +11,23 @@
  */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& in, int inlo, int inhi, vector<int>& post, int postlo, int posthi){
-        if(inlo > inhi) return NULL;
-        TreeNode* root = new TreeNode(post[posthi]);
-        int i = inlo;
-        while(i<inhi){
-            if(in[i] == post[posthi]) break;
+    TreeNode* helper(vector<int> inorder, int inlo, int inhi,vector<int> postorder, int postlo, int posthi){
+        if(inlo>inhi) return NULL;
+        TreeNode* root = new TreeNode(postorder[posthi]);
+        // if(inhi==inlo) return root;
+        int i = inlo ;
+        while(i<=inhi){ //Find the root
+            if(inorder[i]==postorder[posthi]) break;
             i++;
         }
-        int leftdistance = i - inlo;
-        int rightdistance = inhi - i;
-        root->left = helper(in, inlo, i-1, post, postlo, postlo+leftdistance-1);
-        root->right = helper(in, i+1, inhi, post, postlo+leftdistance, posthi-1);
+        int leftDistance = i-inlo;
+        int rightDistance = inhi-i;
+        root->left=helper(inorder,inlo,i-1,postorder,postlo,postlo+leftDistance-1);
+        root->right=helper(inorder,i+1,inhi,postorder,postlo+leftDistance,posthi-1);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = inorder.size();
-        return helper(inorder, 0, n-1, postorder, 0, n-1);
+        return helper(inorder,0,n-1,postorder,0,n-1);
     }
 };
