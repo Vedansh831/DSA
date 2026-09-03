2c6a98fde417b11db6d6fceda39f71aa90e3cf8e
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-09-03 23:28:58 +0530

    Time: 9 ms (5.69%) | Memory: 22.1 MB (5.52%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<list<int>> graph;
+    unordered_set<int> visited;
+    void add_edge(int src, int dest, bool bi_dir = true){
+        graph[src].push_back(dest);
+        if(bi_dir == true){
+            graph[dest].push_back(src); 
+        }
+    }
+    void dfs(int node){
+        visited.insert(node);
+        for(auto nodes : graph[node]){
+            if(visited.count(nodes)==0){
+                dfs(nodes);
+            }
+        }
+    }
+    int connected_components(int v){
+        int result = 0;
+        for(int i=0;i<v;i++){
+            if(visited.count(i)==0){
+                result++;
+                dfs(i);
+            }
+        }
+        return result;
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        int v = isConnected[0].size();
+        graph.resize(v);
+        for(int i = 0; i < v; i++){
+            for(int j = i + 1; j < v; j++){
+                if(isConnected[i][j] == 1){
+                    add_edge(i, j);
+                }
+            }
+        }
+        int ans = connected_components(v);
+        return ans;
+    }
+};
