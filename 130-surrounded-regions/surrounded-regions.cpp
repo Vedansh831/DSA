4156c1af4da5bf27acca46bc41b3c54b4d73eec9
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-09-04 20:18:48 +0530

    Time: 1 ms (29.02%) | Memory: 14.8 MB (30.18%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-
-    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>>& ans){
-        int n = board.size();
-        int m = board[0].size();
-        if(row < 0 || col < 0 || row >= n || col >= m) return;
-        if(board[row][col]!='O' || ans[row][col]!=0) return;
-        ans[row][col] = 1; // 1 means waha pe O aayega
-
-        dfs(row+1, col, board, ans);
-        dfs(row-1, col, board, ans);
-        dfs(row, col+1, board, ans);
-        dfs(row, col-1, board, ans);
-        return;
+    vector<vector<int>> visited;
+    void dfs(int i, int j, vector<vector<int>>& pos, vector<vector<char>>& v){
+        if(i < 0 || j < 0 || i >= pos.size() || j >= pos[0].size()) return;
+        if(visited[i][j]==1) return;
+        visited[i][j]=1;
+        if(v[i][j]=='O') pos[i][j]=2;
+        else return;
+        dfs(i+1, j, pos, v);
+        dfs(i-1, j, pos, v);
+        dfs(i, j+1, pos, v);
+        dfs(i, j-1, pos, v);
     }
-
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-
-        vector<vector<int>> ans(n, vector<int>(m, 0)); // 0 means not occupied
-        // Top Side And Bottom Side
-        for(int i=0;i<n;i++){
-            dfs(i, 0, board, ans);
-            dfs(i, m-1, board, ans);
-        }
-        // Left Side And Right Side
-        for(int i=0;i<m;i++){
-            dfs(0, i, board, ans);
-            dfs(n-1, i, board, ans);
-        }
-
+        visited.resize(n, vector<int>(m, 0));
+        vector<vector<int>> pos(n, vector<int>(m, 0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j] == 'O' && ans[i][j] == 0)board[i][j] = 'X';
+                if(board[i][j]=='X') pos[i][j]=1;
+                else pos[i][j]=0;
             }
         }
-
+        for(int i=0;i<n;i++){
+            if(pos[i][0]==0) dfs(i, 0, pos, board);
+            if(pos[i][m-1]==0) dfs(i, m-1, pos, board);
+        }
+        for(int j=0;j<m;j++){
+            if(pos[0][j]==0) dfs(0, j, pos, board);
+            if(pos[n-1][j]==0) dfs(n-1, j, pos, board);
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(pos[i][j]!=2) board[i][j]='X';
+            }
+        }
         return;
     }
 };
