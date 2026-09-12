f0688a98079f04d475fcb06e741effb4b9ed5901
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-09-12 16:29:36 +0530

    Time: 3 ms (63.47%) | Memory: 18.4 MB (40.71%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // This solution is based on weight sorting using priority queue , but we can also solve it using normal queue data structure 
-        vector<pair<int, int>> graph[n];
+
+        vector<vector<pair<int, int>>> graph(n);
+
         for (auto it : flights) {
             graph[it[0]].push_back({it[1], it[2]});
         }
-        // {cost, {node, flights used}}
-        priority_queue< pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>> > pq;
-        pq.push({0, {src, 0}});
-        // dist[node][flights used]
-        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
-        dist[src][0] = 0;
-        while (!pq.empty()) {
-            auto curr = pq.top();
-            pq.pop();
-            int wt = curr.first;
+
+        queue<pair<int, pair<int, int>>> q; // flights , node, distance
+        q.push({0, {src, 0}});
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+
+            int stops = curr.first;
             int node = curr.second.first;
-            int noK = curr.second.second;
-            if (node == dst) return wt;
-            if (noK == k + 1) continue;
-            for (auto neighbours : graph[node]) {
-                int no = neighbours.first;
-                int w = neighbours.second;
-                int newCost = wt + w;
-                int newFlights = noK + 1;
-                if (newCost < dist[no][newFlights]) {
-                    dist[no][newFlights] = newCost;
-                    pq.push({ newCost, {no, newFlights} });
+            int d = curr.second.second;
+
+            if(stops > k) continue;
+
+            for(auto it : graph[node]){
+                int nextNode = it.first;
+                int distance = it.second;
+                int newDist = distance + d;
+                if(dist[nextNode] > newDist){
+                    dist[nextNode] = newDist;
+                    q.push({stops+1, {nextNode, newDist}});
                 }
             }
         }
-        return -1;
+
+        if(dist[dst] == INT_MAX) return -1;
+        return dist[dst];
     }
 };
