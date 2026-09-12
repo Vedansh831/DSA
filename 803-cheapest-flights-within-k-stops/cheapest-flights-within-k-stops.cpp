70753ace4dbf5b0e176fb7a3c279ac8390c6f3c5
Vedansh Vishwakarma <vedanshvishwakarma831@gmail.com>
2026-09-12 16:06:17 +0530

    Time: 5 ms (37.01%) | Memory: 18.7 MB (26.93%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<pair<int, int>> graph[n];
+        for (auto it : flights) {
+            graph[it[0]].push_back({it[1], it[2]});
+        }
+        // {cost, {node, flights used}}
+        priority_queue< pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>> > pq;
+        pq.push({0, {src, 0}});
+        // dist[node][flights used]
+        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
+        dist[src][0] = 0;
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+            int wt = curr.first;
+            int node = curr.second.first;
+            int noK = curr.second.second;
+            if (node == dst) return wt;
+            if (noK == k + 1) continue;
+            for (auto neighbours : graph[node]) {
+                int no = neighbours.first;
+                int w = neighbours.second;
+                int newCost = wt + w;
+                int newFlights = noK + 1;
+                if (newCost < dist[no][newFlights]) {
+                    dist[no][newFlights] = newCost;
+                    pq.push({ newCost, {no, newFlights} });
+                }
+            }
+        }
+        return -1;
+    }
+};
